aa24bddd00a3c55f3f59812eca27e29bbb236657
Jesus24-Dev <siritjesus24@gmail.com>
2025-05-27 22:48:07 -0400

    first commit

---
+++ src/main/java/com/employeeasistance/employeeasistancemanagement/EmployeeasistancemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.employeeasistance.employeeasistancemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeasistancemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeasistancemanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employeeasistance/employeeasistancemanagement/EmployeeasistancemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeeasistance.employeeasistancemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeasistancemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
