7fa277d86875c108b556de1cbb5b1a8fefa49382
Jesus24-Dev <siritjesus24@gmail.com>
2025-06-01 22:27:36 -0400

    Created Assistance model

--- src/main/java/com/employeeasistance/employeeasistancemanagement/models/Assistance.java
+++ src/main/java/com/employeeasistance/employeeasistancemanagement/models/Assistance.java
@@ -1,13 +1,80 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.employeeasistance.employeeasistancemanagement.models;
 
-/**
- *
- * @author Jesus24-Dev
- */
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "employee_assistance")
 public class Assistance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    
+    @ManyToOne
+    private Employee employee;
+    
+    private LocalDate date;
+    private LocalTime entryTime;    
+    private LocalTime departureTime;
+
+    public Assistance() {
+    }
+
+    public Assistance(UUID id, Employee employee, LocalDate date, LocalTime entryTime, LocalTime departureTime) {
+        this.id = id;
+        this.employee = employee;
+        this.date = date;
+        this.entryTime = entryTime;
+        this.departureTime = departureTime;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getEntryTime() {
+        return entryTime;
+    }
+
+    public void setEntryTime(LocalTime entryTime) {
+        this.entryTime = entryTime;
+    }
+
+    public LocalTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(LocalTime departureTime) {
+        this.departureTime = departureTime;
+    }
+    
     
 }
