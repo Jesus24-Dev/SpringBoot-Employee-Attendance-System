301d8afb0572577e346bcbab6dc58658bd6209ba
Jesus24-Dev <siritjesus24@gmail.com>
2025-06-01 22:18:51 -0400

    Created Employee model

---
+++ src/main/java/com/employeeasistance/employeeasistancemanagement/models/Employee.java
@@ -0,0 +1,82 @@
+
+package com.employeeasistance.employeeasistancemanagement.models;
+
+import com.employeeasistance.employeeasistancemanagement.enums.EmployeePosition;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    
+    private String name;
+    private String email;
+    
+    @Enumerated(EnumType.STRING)
+    private EmployeePosition position;
+    
+    private boolean isActive;
+
+    public Employee() {
+    }
+
+    public Employee(UUID id, String name, String email, EmployeePosition position, boolean isActive) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.position = position;
+        this.isActive = isActive;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public EmployeePosition getPosition() {
+        return position;
+    }
+
+    public void setPosition(EmployeePosition position) {
+        this.position = position;
+    }
+
+    public boolean isIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+    
+    
+}
