87f27701662aa71000fb991a588103399c58cc21
Jesus24-Dev <siritjesus24@gmail.com>
2025-06-01 22:24:11 -0400

    Created Assistance model

---
+++ src/main/java/com/employeeasistance/employeeasistancemanagement/models/Assistance.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.employeeasistance.employeeasistancemanagement.models;
+
+/**
+ *
+ * @author Jesus24-Dev
+ */
+public class Assistance {
+    
+}
