e1c2d3894a51d7890fc52d9b251290fdbec57f39
Jesus24-Dev <siritjesus24@gmail.com>
2025-06-01 22:49:13 -0400

    Create Assistance service with basic CRUD operations.

---
+++ src/main/java/com/employeeasistance/employeeasistancemanagement/services/AssistanceService.java
@@ -0,0 +1,67 @@
+
+package com.employeeasistance.employeeasistancemanagement.services;
+import com.employeeasistance.employeeasistancemanagement.models.Assistance;
+import com.employeeasistance.employeeasistancemanagement.models.Employee;
+import com.employeeasistance.employeeasistancemanagement.repositories.AssistanceRepository;
+import com.employeeasistance.employeeasistancemanagement.repositories.EmployeeRepository;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AssistanceService {
+    private final AssistanceRepository assistanceRepository;
+    private final EmployeeRepository employeeRepository;
+    
+    @Autowired
+    public AssistanceService(AssistanceRepository assistanceRepository, EmployeeRepository employeeRepository){
+        this.assistanceRepository = assistanceRepository;
+        this.employeeRepository = employeeRepository;
+    }
+    
+    public List<Assistance> getAllAssistances(){
+        return assistanceRepository.findAll();
+    }
+    
+    public List<Assistance> getAllAsistancesByEmployee(UUID employeeId){
+        Employee employeeFounded = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new RuntimeException("Employee not found: " + employeeId));
+        
+        return employeeFounded.getEmployeeAssistances();
+    }
+    
+    public void createAssistance(UUID employeeId, LocalDate date, LocalTime entryTime, LocalTime departureTime){
+        Employee employeeFounded = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new RuntimeException("Employee not found: " + employeeId));
+        
+        Assistance newAssistance = new Assistance();
+        
+        newAssistance.setEmployee(employeeFounded);
+        newAssistance.setDate(date);
+        newAssistance.setEntryTime(entryTime);
+        newAssistance.setDepartureTime(departureTime);
+        
+        assistanceRepository.save(newAssistance);
+    }
+    
+    public void updateAssistance(UUID id, LocalDate date, LocalTime entryTime, LocalTime departureTime){
+        Assistance assistanceFounded = assistanceRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Employee not found: " + id));
+        
+        assistanceFounded.setDate(date);
+        assistanceFounded.setEntryTime(entryTime);
+        assistanceFounded.setDepartureTime(departureTime);
+        
+        assistanceRepository.save(assistanceFounded);
+    }
+    
+    public void deleteAssistance(UUID id){
+        Assistance assistanceFounded = assistanceRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Employee not found: " + id));
+        
+        assistanceRepository.delete(assistanceFounded);
+    }
+}
