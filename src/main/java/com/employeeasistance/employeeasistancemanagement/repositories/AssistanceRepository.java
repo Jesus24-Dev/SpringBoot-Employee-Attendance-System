59daa5860a65b527e4dbf4af7120354dafb9befb
Jesus24-Dev <siritjesus24@gmail.com>
2025-06-06 22:31:44 -0400

    created method for check day and employee register

--- src/main/java/com/employeeasistance/employeeasistancemanagement/repositories/AssistanceRepository.java
+++ src/main/java/com/employeeasistance/employeeasistancemanagement/repositories/AssistanceRepository.java
@@ -2,6 +2,7 @@
 package com.employeeasistance.employeeasistancemanagement.repositories;
 
 import com.employeeasistance.employeeasistancemanagement.models.Assistance;
+import com.employeeasistance.employeeasistancemanagement.models.Employee;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
@@ -9,4 +10,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AssistanceRepository extends JpaRepository<Assistance, UUID>{
     List<Assistance> findByDate(LocalDate date);
+    boolean existsByDateAndEmployee(LocalDate date, Employee employee);
 }
