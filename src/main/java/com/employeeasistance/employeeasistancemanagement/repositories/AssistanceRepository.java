579a366a0823723fdc5ac544c27f14cb3d1da5a0
Jesus24-Dev <siritjesus24@gmail.com>
2025-06-06 22:16:29 -0400

    Created custom method to find assistances by date

--- src/main/java/com/employeeasistance/employeeasistancemanagement/repositories/AssistanceRepository.java
+++ src/main/java/com/employeeasistance/employeeasistancemanagement/repositories/AssistanceRepository.java
@@ -2,9 +2,11 @@
 package com.employeeasistance.employeeasistancemanagement.repositories;
 
 import com.employeeasistance.employeeasistancemanagement.models.Assistance;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AssistanceRepository extends JpaRepository<Assistance, UUID>{
-    
+    List<Assistance> findByDate(LocalDate date);
 }
