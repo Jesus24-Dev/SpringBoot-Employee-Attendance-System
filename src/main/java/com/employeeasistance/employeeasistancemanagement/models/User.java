d99388893fdafd2d4dacbd52f8884211e720d8d6
Jesus24-Dev <siritjesus24@gmail.com>
2025-06-09 17:08:40 -0400

    created user model for login and security

---
+++ src/main/java/com/employeeasistance/employeeasistancemanagement/models/User.java
@@ -0,0 +1,114 @@
+
+package com.employeeasistance.employeeasistancemanagement.models;
+
+import com.employeeasistance.employeeasistancemanagement.enums.UserRoles;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Table(name = "user_config")
+public class User implements UserDetails{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    
+    private String username;
+    private String password;
+    
+    private UserRoles role;
+    
+    @OneToOne
+    private Employee employee;
+
+    public User() {
+    }
+
+    public User(UUID id, String username, String password, UserRoles role, Employee employee) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.employee = employee;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String name) {
+        this.username = name;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRoles getRole() {
+        return role;
+    }
+
+    public void setRole(UserRoles role) {
+        this.role = role;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    
+    
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked(); 
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired(); 
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled(); 
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+ 
+}
