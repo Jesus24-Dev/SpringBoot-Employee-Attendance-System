db1d06749831f976f9f30fedb01d8f9c50052433
Jesus24-Dev <siritjesus24@gmail.com>
2025-06-10 14:42:29 -0400

    refactor Employee model to establish proper relationship with User and enhance cascade settings

--- src/main/java/com/employeeasistance/employeeasistancemanagement/models/Employee.java
+++ src/main/java/com/employeeasistance/employeeasistancemanagement/models/Employee.java
@@ -2,6 +2,7 @@
 package com.employeeasistance.employeeasistancemanagement.models;
 
 import com.employeeasistance.employeeasistancemanagement.enums.EmployeePosition;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -9,6 +10,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -34,7 +36,8 @@ public class Employee {
     @OneToMany(mappedBy = "employee", fetch = FetchType.LAZY)
     private List<Assistance> employeeAssistances;
     
-    @OneToOne(mappedBy="employee")
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
 
     public Employee() {
