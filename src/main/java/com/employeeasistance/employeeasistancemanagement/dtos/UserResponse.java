e5e9f8a7567cc3c3c11c4a2b2fde036710fc960d
Jesus24-Dev <siritjesus24@gmail.com>
2025-06-11 12:06:52 -0400

    refactor UserResponse to include id and employee fields with corresponding getters

--- src/main/java/com/employeeasistance/employeeasistancemanagement/dtos/UserResponse.java
+++ src/main/java/com/employeeasistance/employeeasistancemanagement/dtos/UserResponse.java
@@ -3,14 +3,19 @@ package com.employeeasistance.employeeasistancemanagement.dtos;
 
 import com.employeeasistance.employeeasistancemanagement.enums.UserRoles;
 import com.employeeasistance.employeeasistancemanagement.models.User;
+import java.util.UUID;
 
 public class UserResponse {
+    private final UUID id;
     private final String username;
     private final UserRoles role;
+    private final EmployeeMinResponse employee;
     
     public UserResponse(User user){
+        this.id = user.getId();
         this.username = user.getUsername();
         this.role = user.getRole();
+        this.employee = user.getEmployee() != null ? new EmployeeMinResponse(user.getEmployee()) : null;
     }
 
     public String getUsername() {
@@ -21,5 +26,11 @@ public class UserResponse {
         return role;
     }
     
+    public UUID getId(){
+        return id;
+    }
     
+    public EmployeeMinResponse getEmployee(){
+        return employee;
+    }
 }
