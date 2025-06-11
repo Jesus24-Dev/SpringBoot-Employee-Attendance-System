2d74ae4c5d488fbd9fe3e64dad72a8956d02fe9e
Jesus24-Dev <siritjesus24@gmail.com>
2025-06-11 12:08:13 -0400

    refactor JwtUserDetailsService to simplify loadUserByUsername method

--- src/main/java/com/employeeasistance/employeeasistancemanagement/services/JwtUserDetailsService.java
+++ src/main/java/com/employeeasistance/employeeasistancemanagement/services/JwtUserDetailsService.java
@@ -9,19 +9,15 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class JwtUserDetailsService implements UserDetailsService{
-    
+public class JwtUserDetailsService implements UserDetailsService {
+
     @Autowired
     private UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) {
-         return userRepository.findByUsername(username)
-                .map(user -> new org.springframework.security.core.userdetails.User(
-                        user.getUsername(),
-                        user.getPassword(),
-                        user.getAuthorities()))
-                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
     }
-    
 }
+
