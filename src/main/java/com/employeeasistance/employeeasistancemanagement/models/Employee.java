11faba99c457e6cd0cc3646518a491a071b5a8ed
Jesus24-Dev <siritjesus24@gmail.com>
2025-06-11 12:06:55 -0400

    refactor Employee entity to remove cascade option from user relationship

--- src/main/java/com/employeeasistance/employeeasistancemanagement/models/Employee.java
+++ src/main/java/com/employeeasistance/employeeasistancemanagement/models/Employee.java
@@ -36,7 +36,7 @@ public class Employee {
     @OneToMany(mappedBy = "employee", fetch = FetchType.LAZY)
     private List<Assistance> employeeAssistances;
     
-    @OneToOne(cascade = CascadeType.ALL)
+    @OneToOne
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
 
