a000102b77c6bcd942dd1f4504afc131de06d3da
Jesus24-Dev <siritjesus24@gmail.com>
2025-06-11 12:08:17 -0400

    refactor UserService to implement user retrieval and management methods

--- src/main/java/com/employeeasistance/employeeasistancemanagement/services/UserService.java
+++ src/main/java/com/employeeasistance/employeeasistancemanagement/services/UserService.java
@@ -4,6 +4,8 @@ package com.employeeasistance.employeeasistancemanagement.services;
 import com.employeeasistance.employeeasistancemanagement.enums.UserRoles;
 import com.employeeasistance.employeeasistancemanagement.models.User;
 import com.employeeasistance.employeeasistancemanagement.repositories.UserRepository;
+import java.util.List;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,17 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
     
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+    
+    public User getUserById(UUID id){
+        User userFounded = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not founded: " + id));
+        
+        return userFounded;
+    }
+    
     public User createUser(String username, String password, UserRoles role){
         User userToSave = new User();
         userToSave.setUsername(username);
@@ -27,5 +40,21 @@ public class UserService {
         userToSave.setRole(role);
         
         return userRepository.save(userToSave);
+    }
+    
+    public User updateUser(UUID id, String username, String password, UserRoles role){
+        User userToUpdate = getUserById(id);
+        
+        userToUpdate.setUsername(username);
+        userToUpdate.setPassword(passwordEncoder.encode(password));
+        userToUpdate.setRole(role);
+        
+        return userRepository.save(userToUpdate);
+    }
+    
+    public void deleteUser(UUID id){
+        User userToDelete = getUserById(id);
+        
+        userRepository.delete(userToDelete);
     }
 }
