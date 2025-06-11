0da5369f0ad3a84c88956b4686696e8437ff6e7f
Jesus24-Dev <siritjesus24@gmail.com>
2025-06-11 12:08:08 -0400

    refactor EmployeeService to include user authentication and associate employee with current user

--- src/main/java/com/employeeasistance/employeeasistancemanagement/services/EmployeeService.java
+++ src/main/java/com/employeeasistance/employeeasistancemanagement/services/EmployeeService.java
@@ -3,20 +3,32 @@ package com.employeeasistance.employeeasistancemanagement.services;
 
 import com.employeeasistance.employeeasistancemanagement.enums.EmployeePosition;
 import com.employeeasistance.employeeasistancemanagement.models.Employee;
+import com.employeeasistance.employeeasistancemanagement.models.User;
 import com.employeeasistance.employeeasistancemanagement.repositories.EmployeeRepository;
+import com.employeeasistance.employeeasistancemanagement.repositories.UserRepository;
+import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
     
     private final EmployeeRepository employeeRepository;
+    private final UserRepository userRepository;
     
     @Autowired
-    public EmployeeService(EmployeeRepository employeeRepository){
+    public EmployeeService(EmployeeRepository employeeRepository, UserRepository userRepository){
         this.employeeRepository = employeeRepository;
+        this.userRepository = userRepository;
+    }
+    
+    public User getAuthenticatedUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return (User) auth.getPrincipal();
     }
     
     public List<Employee> getAllEmployees(){
@@ -30,13 +42,24 @@ public class EmployeeService {
         return employeeFounded;
     }
     
+    @Transactional
     public Employee createEmployee(String name, String email, EmployeePosition position){
+               
+        User authUser = getAuthenticatedUser();
+
+        User currentUser = userRepository.findById(authUser.getId())
+            .orElseThrow(() -> new RuntimeException("User not founded"));
+
+        if (currentUser.getEmployee() != null) {
+            throw new IllegalStateException("This user already have a employee profile.");
+        }
+
         Employee employeeCreated = new Employee();
-        
         employeeCreated.setName(name);
         employeeCreated.setEmail(email);
         employeeCreated.setPosition(position);
         employeeCreated.setIsActive(true);
+        employeeCreated.setUser(currentUser);
         
         return employeeRepository.save(employeeCreated);
     }
