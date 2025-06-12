f3b03800bc9685c623bd5989cbef25b67a0e732e
Jesus24-Dev <siritjesus24@gmail.com>
2025-06-11 22:00:35 -0400

    fix custom query. Add Spring query

--- src/main/java/com/employeeasistance/employeeasistancemanagement/repositories/AssistanceRepository.java
+++ src/main/java/com/employeeasistance/employeeasistancemanagement/repositories/AssistanceRepository.java
@@ -7,15 +7,10 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 
 public interface AssistanceRepository extends JpaRepository<Assistance, UUID>{
     List<Assistance> findByDate(LocalDate date);
-    boolean existsByDateAndEmployee(LocalDate date, Employee employee);
-    
-    @Query("SELECT a from assistance WHERE a.date BETWEEN :startDate AND :finishDate")
-    List<Assistance> findAssistanceBetweenDates(
-            @Param("startDate") LocalDate startDate,
-            @Param("finishDate") LocalDate finishDate);
+    boolean existsByDateAndEmployee(LocalDate date, Employee employee);   
+    List<Assistance> findByDateBetween(LocalDate startDate, LocalDate endDate);
 }
